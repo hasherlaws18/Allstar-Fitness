a7f4cca73e4119f4dca4f512122e80fe6fd224ae
conradkg1 <conradkg1@gmail.com>
2023-03-27 21:30:19 -0500

    loginlogout

---
+++ public/js/logout.js
@@ -0,0 +1,14 @@
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+    });
+
+    if(response.ok) {
+        document.location.replace('/login');
+    } else {
+        alert('Failed to logout');
+    }
+};
+
+document.querySelector('#logout').aaddEventListener('click', logout);
