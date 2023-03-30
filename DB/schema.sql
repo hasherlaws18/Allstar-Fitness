cc97443f9d0db9a7dcd8a20b48269bae34aa8775
Julianne <juliaguas17@gmail.com>
2023-03-29 19:25:14 -0500

    changes to homeRoutes - layouts

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,9 +1,2 @@
 DROP DATABASE IF EXISTS user_db;
 CREATE DATABASE user_db;
-
-USE user_db;
-
-CREATE TABLE Steps (
-     weekly INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     Steps VARCHAR(30) NOT NULL
-)
